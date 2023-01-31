3d61f30995b10762b7d8f6addfd63ddfa6b00629
mkellner43 <matthew.kellner@outlook.com>
2023-01-31 13:52:38 -0600

    updated UI

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,57 +1,44 @@
 import React, { useState, useEffect } from "react";
 import './style.scss';
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, easeInOut } from "framer-motion";
 
 const Cart = ({cart, deleteFromCart}) => {
   const [display, setDisplay] = useState(false)
   const [cartNumber, setCartNumber] = useState(0)
-
-  const handleClick = () => {
-    setDisplay(prevDisplay => !prevDisplay)
-  }
-  const cartDisplay = () => {
-    const modifiableCart = cart
-    const cartItems = modifiableCart.map((cartItem, i) => {
-      return (
-        <motion.div className="cart-item" key={`${cartItem.item.id}${i}`}>
-          <p>{cartItem.amount}</p>
-          <img src={cartItem.item.image} alt={cartItem.item.name}/>
-          <div>
-            <p>{cartItem.item.name}</p>
-            <p>{cartItem.item.price}</p>
-          </div>
-          <motion.i 
-            className="fa-solid fa-xmark delete-cart-item" 
-            onClick={() => deleteFromCart(cartItem)}/>
-        </motion.div>
-      )
-    })
-    return <div className="cart-items">{cartItems}</div>
-  }
-
-  useEffect(() => {
-    let modCart = cart
-    setCartNumber(() => 
-      modCart.length > 0 ? modCart.map(item => Number(item.amount)).reduce(
-        (prev, current) => prev + current
-      ) : 0
-    )
-  }, [cart])
-
   const variant = {
     initial: {
       y: -500
     },
     animate: {
-      y: 0
+      y: 0,
+      transition: {
+        staggerChildren: 0.15
+      }
     },
     exit: {
       y: -500,
       transition: {
-        duration: 1
+        staggerChildren: 0.15,
       }
     }
   }
+  const var3 = {
+    initial: {
+      y: -500,
+      opacity: 0
+    },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: .75
+      }
+    },
+    exit: {
+      y: -500,
+      opacity: 0
+    }
+   }
    const var2 = {
     initial: {
       y: -100
@@ -63,6 +50,47 @@ const Cart = ({cart, deleteFromCart}) => {
       y: -100,
     }
    }
+
+
+  const handleClick = () => {
+    setDisplay(prevDisplay => !prevDisplay)
+  }
+  const cartDisplay = () => {
+    const modifiableCart = cart
+    const cartItems = modifiableCart.map((cartItem, i) => {
+      return (
+          <div className="cart-item" key={`${cartItem.item.id}${i}`}>
+            <p>{cartItem.amount}</p>
+            <img src={cartItem.item.image} alt={cartItem.item.name}/>
+            <div>
+              <p>{cartItem.item.name}</p>
+              <p>{cartItem.item.price}</p>
+            </div>
+            <motion.i 
+              className="fa-solid fa-xmark delete-cart-item" 
+              onClick={() => deleteFromCart(cartItem)}/>
+          </div>
+      )
+    })
+    return <motion.div
+        variants={var3}
+        className="cart-items"
+      >
+        {cartItems}
+      </motion.div>
+
+  }
+
+  useEffect(() => {
+    let modCart = cart
+    setCartNumber(() => 
+      modCart.length > 0 ? modCart.map(item => Number(item.amount)).reduce(
+        (prev, current) => prev + current
+      ) : 0
+    )
+  }, [cart])
+
+
   return (
     <>
     <AnimatePresence>
