48b02e610fa38263beeda1102c8d884c64206d5a
mkellner43 <matthew.kellner@outlook.com>
2023-02-03 10:17:40 -0600

    updated nav animation

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './menu.scss';
 import Hamburger from '../Hamburger';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Menu = ({isOpen, setIsOpen}) => {
   return (
     <>
       <nav className='nav-small'>
-        <Hamburger isOpen={isOpen} setIsOpen={setIsOpen} />
+        <Hamburger isOpen={isOpen} setIsOpen={setIsOpen}/>
+        <AnimatePresence>
         { isOpen &&
-        <motion.div 
-          animate={{y: [-200, 0]}}
-          className='nav-links'>
-          <Link to="/">Home</Link>
-          <Link to="/shop">Shop</Link>
-          <Link to="/about-us">About Us</Link>
-         </motion.div>
+          <motion.div
+            initial={{y: -200}}
+            animate={{y: 0, transition: {duration: 0.5}}}
+            exit={{y: -200, transition: {duration: 0.5}}}
+            className='nav-links'>
+            <Link to="/">Home</Link>
+            <Link to="/shop">Shop</Link>
+            <Link to="/about-us">About Us</Link>
+          </motion.div>
         }
+        </AnimatePresence>
       </nav>
       <nav className='nav-normal'>
         <Link to="/">Home</Link>
